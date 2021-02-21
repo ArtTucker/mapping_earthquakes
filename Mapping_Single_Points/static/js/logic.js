6a268de301d8d865f6f0b11463ab2ec9c9e846af
Art Tucker <art.w.tucker@gmail.com>
2021-02-21 00:10:14 -0800

    Add marker for London, circle for LA

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -4,8 +4,16 @@ console.log("working");
 // Create the map object with a center and zoom level.
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
-//  Add a marker to the map for Los Angeles, California.
-let marker = L.marker([34.0522, -118.2437]).addTo(map);
+// add a marker to the map for London, UK
+var marker = L.marker([51.5, -0.09]).addTo(mymap);
+
+//  Add a circle to the map over Los Angeles, California.
+let marker = L.circle([34.0522, -118.2437], {
+    color: 'black',
+    fillColor: "yellow",
+    fillOpacity: 0.60,
+    radius: 500
+}).addTo(map);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -16,3 +24,5 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
+
