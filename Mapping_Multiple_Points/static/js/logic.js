0bff76f5beb657a01770aa5599c24362285ba518
Art Tucker <art.w.tucker@gmail.com>
2021-02-22 14:23:31 -0800

    change city markers to circleMarkers

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -10,7 +10,12 @@ let cityData = cities;
 // Loop through the cities array and create one marker for each city.
 cityData.forEach(function(city){
     //console.log(city)
-    L.marker(city.location)
+    L.circleMarker(city.location, {
+        radius: city.population/200000,
+        color: 'orange',
+        fillColor: 'orange',
+        weight: 4
+    })
         .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
         .addTo(map);
 });
