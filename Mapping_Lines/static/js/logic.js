24f80c8fe939944707b59e62789fea1a22ca1001
Art Tucker <art.w.tucker@gmail.com>
2021-02-22 19:00:33 -0800

    Create a line

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -2,7 +2,18 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([36.1733, -120.1794], 7);
+
+// Coordinates for each point to be used in the line.
+let line = [
+    [33.9416, -118.4085],
+    [51.47, -0.4582]
+];
+
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+    color: "red"
+}).addTo(map);
 
 // Get data from cities.js
 let cityData = cities;
