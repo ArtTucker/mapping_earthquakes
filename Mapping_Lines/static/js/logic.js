8a31875884f70db99a1df74f5a0639033e5dea71
Art Tucker <art.w.tucker@gmail.com>
2021-02-22 22:10:55 -0800

    Add new nodes to same route line

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -4,10 +4,12 @@ console.log("working");
 // Create the map object with a center and zoom level.
 let map = L.map('mapid').setView([36.1733, -120.1794], 7);
 
-// Coordinates for each point to be used in the line.
+// Coordinates for each point to be used in the polyline.
 let line = [
     [33.9416, -118.4085],
-    [37.6213, -122.3790]
+    [37.6213, -122.3790],
+    [40.7899, -111.9791],
+    [47.4502, -122.3088]
 ];
 
 // Create a polyline using the line coordinates and make the line red.
