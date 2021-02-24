34dd07aea6664322896fc3ee45597201f464e2f2
Art Tucker <art.w.tucker@gmail.com>
2021-02-23 21:28:36 -0800

    create popup on marker

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -2,7 +2,7 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+let map = L.map('mapid').setView([37.5, -122.5], 10);
 
 // Add GeoJSON data.
 let sanFranAirport = {
@@ -27,8 +27,18 @@ let sanFranAirport = {
     }]
 };
 
+// Grabbing our GeoJSON data
+L.geoJson(sanFranAirport, {
+    // We turn each feature into a marker on the map
+    pointToLayer: function(feature, latlng) {
+        console.log(feature);
+        return L.marker(latlng)
+        .bindPopup("<h2>" + feature.properties.name + "</h2> <hr> <h2>"+ feature.properties.city + ", " + feature.properties.country + "</h2>");
+    }
+}).addTo(map);
+
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
